a0e57c5d322bb7b8965d7d685ea53c064a017c8e
temchikS <148534802+temchikS@users.noreply.github.com>
2024-03-19 18:37:52 +0600

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,5 +48,27 @@ namespace bower
                 }
             }
         }
+        private void Nazad(object sender, RoutedEventArgs e)
+        {
+            webView.GoBack();
+        }
+
+        private void Vpered(object sender, RoutedEventArgs e)
+        {
+            webView.GoForward();
+        }
+        private void webView_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            // Получаем URL-адрес, на который пытается перейти браузер
+            string targetUrl = e.Uri;
+
+            // Проверяем, является ли URL-адрес целевой страницей (например, "https://example.com")
+            if (!targetUrl.StartsWith("https://www.google.com/"))
+            {
+                // Блокируем навигацию на другие страницы
+                e.Cancel = true;
+            }
+        }
+
     }
 }
